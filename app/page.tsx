1057a3af1248e43264ec6cbd767bdfd65eda76d0
tpessoa <tiagopessoa123@hotmail.com>
2023-02-25 23:05:44 +0000

    add text to speech

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 
 import Input from "@/components/Input";
 import Meaning from "@/components/Meaning";
+import TextToSpeech from "@/components/TextToSpeech";
 import { DictionaryAPI } from "@/types/dictionaryapi";
-import { useEffect, useState } from "react";
-import { RiPlayFill } from "react-icons/ri";
+import { useState } from "react";
 
 export default function Home() {
   const [data, setData] = useState<DictionaryAPI[]>([]);
@@ -52,11 +52,7 @@ export default function Home() {
                 {response?.phonetic}
               </div>
             </div>
-            <div className="absolute top-1/2 right-0 transform -translate-x-1/2 -translate-y-1/2">
-              <div className="flex justify-center w-16 h-16 bg-purple-400/30 dark:bg-purple-400/10 rounded-full relative">
-                <RiPlayFill className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-purple-500 w-8 h-8" />
-              </div>
-            </div>
+            <TextToSpeech text={response?.word} />
           </div>
           {response?.meanings.map((meaning, index) => (
             <Meaning key={index} {...meaning} />
---
+++ components/TextToSpeech.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { RiPlayFill } from "react-icons/ri";
+
+const synth = window.speechSynthesis;
+
+type TextToSpeechProps = {
+  text: string;
+};
+
+const TextToSpeech = ({ text }: TextToSpeechProps) => {
+  const speak = () => {
+    if (synth.speaking) {
+      console.error("Already speaking...");
+      return;
+    }
+
+    if (text) {
+      const speakText = new SpeechSynthesisUtterance(text);
+
+      speakText.onend = (e) => {
+        console.log("Done speaking...", e);
+      };
+
+      speakText.onerror = (e) => {
+        console.log("Something went wrong.", e);
+      };
+      // "Microsoft Zira - English (United States)"
+      speakText.voice = synth.getVoices().at(2)!;
+
+      synth.speak(speakText);
+    }
+  };
+
+  return (
+    <div className="absolute top-1/2 right-0 transform -translate-x-1/2 -translate-y-1/2">
+      <button className="flex justify-center w-16 h-16 bg-purple-400/30 dark:bg-purple-400/10 rounded-full relative">
+        <RiPlayFill
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-purple-500 w-8 h-8"
+          onClick={speak}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default TextToSpeech;
